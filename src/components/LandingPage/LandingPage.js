7c35b38abf3ff2319367935e8be3f1ecaed8c8c2
northersouth <djli@knox.edu>
2022-04-13 13:52:02 -0500

    button to send user to log in panel

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,15 +1,29 @@
 import "./LandingPage.css";
+import React from 'react';
+import './App.css';
+import {ButtonComponent} from '@syncfusion/ej2-react-buttons';
 
 const LandingPage = () => {
+function App() {
+  const SaySomething = (args) => {
+    print("Saying Something!")
+  }
   return (
-    <div>
+    <div className>
       {/** 
       Signup and Login Buttons
       Create your own components for Sign up and Login buttons Then add them
       to this section You can add to the LandingPage.css file for styling 
+      */
+      <ButtonComponent cssClass="e-info"
+                       onClick={SaySomething}>
+        content = "Log In"
+      </ButtonComponent>
       
       
-      */}
+
+      
+      }
       {
         //Logo
       }
@@ -19,6 +33,7 @@ const LandingPage = () => {
       }
     </div>
   );
+}
 };
 
 export default LandingPage;
