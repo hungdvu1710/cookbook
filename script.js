0e531d4aa9c764b2ecca194984c7337b6f28240d
Dakota <djli@knox.edu>
2022-04-25 14:14:19 -0500

    file with the edamam key info

---
+++ script.js
@@ -0,0 +1,20 @@
+let searchButton = document.querySelector("#search")
+
+//Add an event listener to the button that runs the function sendApiRequest when it is clicked
+searchButton.addEventListener("click", ()=>{
+    console.log("button pressed")
+    sendApiRequest()
+})
+
+//An asynchronous function to fetch data from the API.
+async function sendApiRequest(){
+    let APP_ID = "98817906"
+    let API_KEY = "5bdef1c2cd6643063f7313d060069af6"
+    let response = await fetch('');
+    console.log(response)
+}
+
+//function that does something with the data recieved from the API.
+function useApiData(data){
+
+}
