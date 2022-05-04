73b456a463f3a1346cc213a7cb54af2f03981c38
Hung Vu <hungvsme@gmail.com>
2022-05-03 22:20:49 -0500

    Edit Search Bar CSS

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,15 +6,6 @@ import RecipeCard from "../RecipeCard";
 
 const BE_HOST = process.env.REACT_APP_BACKEND_DOMAIN;
 
-// fetch(BE_HOST + "/api/recipes", {
-//   body: JSON.stringify(userData),
-//   cache: "no-cache",
-//   mode: "cors",
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
 const LandingPage = () => {
   const [searchResult, setSearchResult] = useState([]);
   const { isSignedIn, user } = useUser();
@@ -37,17 +28,6 @@ const LandingPage = () => {
   }
   return (
     <div>
-      {/** 
-      Signup and Login Buttons
-      Create your own components for Sign up and Login buttons Then add them
-      to this section You can add to the LandingPage.css file for styling 
-      
-      
-      */}
-
-      {
-        //Logo
-      }
       <SearchBar placeholder="Start browsing for recipes!" setSearchResult={setSearchResult} />
       {
         searchResult.map(result => {
@@ -57,58 +37,5 @@ const LandingPage = () => {
     </div>
   );
 };
-
-async function SendApiRequest(inputVal) {
-  let APP_ID = "98817906";
-  let API_KEY = "5bdef1c2cd6643063f7313d060069af6";
-  let response = await fetch(
-    "https://api.edamam.com/api/recipes/v2?type=public&q=" +
-      inputVal +
-      "&app_id=" +
-      APP_ID +
-      "&app_key=" +
-      API_KEY +
-      "&random=true"
-  );
-  let data = await response.json();
-  console.log(data);
-  //RecipeCard(data)
-  const { hits } = data;
-  hits.forEach((hit) => {
-    const { recipe } = hit;
-    const { image, label, totalTime, url, mealType } = recipe;
-    console.log(image, label, totalTime, url, mealType);
-  });
-
-  return;
-}
-
-//function that does something with the data recieved from the API.
-/*function useApiData(data){
-  document.querySelector("#content").innerHTML = `
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    `
-  );
-}*/
 
 export default LandingPage;
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -3,53 +3,77 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { AccessTime } from "@mui/icons-material";
-import Link from '@mui/material/Link';
-import './RecipeCard.css'
+import Link from "@mui/material/Link";
+import { Button } from "@mui/material";
+import { useUser } from "@clerk/clerk-react";
+import "./RecipeCard.css";
+
+const BE_HOST = process.env.REACT_APP_BACKEND_DOMAIN;
+
+// fetch(BE_HOST + "/api/recipes", {
+//   body: JSON.stringify(userData),
+//   cache: "no-cache",
+//   mode: "cors",
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
 const RecipeCard = (props) => {
-  return <Grid item xs={3} className="recipe-card">
-    <Paper elevation={3}>
-      <img
-        src={props.recipe.image}
-        alt="..."
-        className="img"
-      />
-      <Box paddingX={1}>
-        <Typography variant="subtitle1" component="h2">
-          {props.recipe.label}
-        </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center"
-          }}
-        >
-          <AccessTime sx={{ width: 12.5 }} />
-          <Typography variant="body2" component="p" marginLeft={0.5}>
-            {props.recipe.totalTime} Minutes
+  const { user } = useUser();
+  const { id } = user;
+  const saveRecipe = () => {
+    const recipeData = {
+      recipe: props.recipe,
+      id: id
+    }
+    fetch(BE_HOST + "/api/recipes", {
+      body: JSON.stringify(recipeData),
+      cache: "no-cache",
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  return (
+    <Grid item xs={3} className="recipe-card">
+      <Paper elevation={3}>
+        <img src={props.recipe.image} alt="..." className="img" />
+        <Box paddingX={1}>
+          <Typography variant="subtitle1" component="h2">
+            {props.recipe.label}
           </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              {props.recipe.totalTime} Minutes
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              Meal Type: {props.recipe.mealType[0]}
+            </Typography>
+          </Box>
+          <Link underline="hover" variant="body2" href={props.recipe.url}>
+            GO TO RECIPE!!!
+          </Link>
         </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center"
-          }}
-        >
-          <Typography variant="body2" component="p" marginLeft={0.5}>
-            Meal Type: {props.recipe.mealType[0]}
-          </Typography>
-
-        </Box>
-        <Link
-          underline="hover"
-          variant="body2"
-          href={props.recipe.url}
-        >
-          GO TO RECIPE!!!
-        </Link>
-      </Box>
-    </Paper>
-  </Grid>
+      </Paper>
+    </Grid>
+  );
 };
 
 export default RecipeCard;
