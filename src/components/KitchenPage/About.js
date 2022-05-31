47d6075a2011f2c54c2e54b6f2ed892723eeb93e
Hung Vu <hungvsme@gmail.com>
2022-05-30 22:45:03 -0500

    Fix Bugs in About Page

--- src/components/KitchenPage/About.js
+++ src/components/KitchenPage/About.js
@@ -10,7 +10,7 @@ export const About = () => {
 
         <img src={pic} alt="pic" id="pic" />
 
-        <p>
+        <div>
           <h2> No more walls of texts. Straight to the recipe. </h2>
 
           <h2>
@@ -22,25 +22,22 @@ export const About = () => {
           <h2>
             {""}
             We use the recipe Search API from Edamam. Visit{" "}
-            <link> https://developer.edamam.com/edamam-recipe-api</link> for
-            more info.
+            <a href="https://developer.edamam.com/edamam-recipe-api">https://developer.edamam.com/edamam-recipe-api</a>
+            {" "} for more info.
           </h2>
 
-          <div>
           <img src={ed} alt="ed" id="ed" />
-
-          </div>
 
           <h2>
             We hope you love this app as much as we loved making it! Happy
             cooking!!
           </h2>
 
+          <h3> Best,</h3>
           <h2>
-            <p> Best,</p>
             Aakriti, Dakota, Hung :)
           </h2>
-        </p>
+        </div>
       </div>
     </div>
   );
